76331d58f259c68b68e7b4a121f9faec9e191070
ODG DimanODG <dimanodg@mail.ru>
2017-09-01 09:25:37 +0300

    Commit update FormPost from add new Post (correct errors)

--- blog/views.py
+++ blog/views.py
@@ -16,6 +16,6 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
-    form=PostForm()
-    return render(request, 'blog/post_edit.html', {'post': form})
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
 
