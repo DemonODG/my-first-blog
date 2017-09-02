fbbc6cbbd71eb4bef7b6a41a6495f46db525e386
ODG DimanODG <dimanodg@mail.ru>
2017-09-02 14:54:27 +0300

    Commit add Comment model from my blog. Add form to append comments from blog

--- blog/models.py
+++ blog/models.py
@@ -7,13 +7,13 @@ from django.utils import timezone
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    text_short=models.TextField(blank=True)
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    text = models.TextField(verbose_name='Текст поста')
+    text_short=models.TextField(blank=True, verbose_name='Краткое содержание')
     created_date = models.DateTimeField(
-            default=timezone.now)
+            default=timezone.now, verbose_name='Дата создания')
     published_date = models.DateTimeField(
-            blank=True, null=True)
+            blank=True, null=True, verbose_name='Дата публикации')
 
     def publish(self):
         self.published_date = timezone.now()
@@ -21,3 +21,18 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.CharField(max_length=200, verbose_name='Автор')
+    text = models.TextField(verbose_name='Комментарий')
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
+
